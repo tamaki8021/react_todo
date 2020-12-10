c34bba19f71b7b634b89456d39c0caf4676730b6
tamaki <hbk2002320@gmail.com>
2020-12-10 12:37:41 +0900

    conect firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import { FormControl, TextField } from '@material-ui/core';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import { db } from './firebase';
+import addToPhotosIcon from "@material-ui/icons/AddAPhotos";
 
-function App() {
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState([{ id: "", title: "" }]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    const unSub = db.collection("tasks").onSnapshots((snapshot) => {
+      setTasks(
+        snapshot.docs.map((doc: any) => ({id: doc.id, title: doc.data().title}))
+      );
+    });
+    return () => unSub();
+  }, []);
+
+  const newTask = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    db.collection("tasks").add({title: input});
+    setInput("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App by React/Firebase</h1>
+
+      <FormControl>
+        <TextField
+            InputLabelProps={{
+              shrink: true, //右上に表示
+            }}
+            label="New task ?"
+            value={input}
+            onChange={(e: React.ChangeEvent<HEMLInputElement>) =>
+              setInput(e.target.value)
+             }
+        />
+      </FormControl>
+      <button disabled={!input} onClick={newTask}>
+        <addToPhotosIcon />
+      </button>
+
+      {tasks.map((task) => 
+        <h3 key={task.id}>{task.title}</h3>
+      )}
     </div>
   );
 }
