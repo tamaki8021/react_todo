eba050701d1a35bae9b91b3996591d279edb67e8
tamaki <hbk2002320@gmail.com>
2021-02-16 01:01:21 +0900

    initial-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,39 @@
-import { FormControl, TextField } from '@material-ui/core';
-import React, {useState, useEffect} from 'react';
-import './App.css';
-import { db } from './firebase';
+// import { FormControl, TextField } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { db } from "./firebase";
 // import addToPhotosIcon from "@material-ui/icons/AddAPhotos";
 
 const App: React.FC = () => {
   const [tasks, setTasks] = useState([{ id: "", title: "" }]);
-  const [input, setInput] = useState("");
+  // const [input, setInput] = useState("");
 
   useEffect(() => {
-    const unSub = db.collection("tasks").onSnapshots((snapshot) => {
+    const unSub = db.collection("tasks").onSnapshot((snapshot) => {
       setTasks(
-        snapshot.docs.map((doc: any) => ({id: doc.id, title: doc.data().title}))
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          title: doc.data().title,
+        }))
       );
     });
     return () => unSub();
   }, []);
 
-  const newTask = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    db.collection("tasks").add({title: input});
-    setInput("");
-  }
+  // const newTask = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  //   db.collection("tasks").add({ title: input });
+  //   setInput("");
+  // };
 
   return (
     <div className="App">
       <h1>Todo App by React/Firebase</h1>
 
-      <FormControl>
-        <TextField
-            InputLabelProps={{
-              shrink: true, //右上に表示
-            }}
-            label="New task ?"
-            value={input}
-            onChange={(e: React.ChangeEvent<HEMLInputElement>) =>
-              setInput(e.target.value)
-             }
-        />
-      </FormControl>
-      <button disabled={!input} onClick={newTask}>
-        {/* <addToPhotosIcon /> */}
-      </button>
-
-      {tasks.map((task) => 
+      {tasks.map((task) => (
         <h3 key={task.id}>{task.title}</h3>
-      )}
+      ))}
     </div>
   );
-}
+};
 
 export default App;
