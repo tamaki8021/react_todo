5e5ea15d45375c9e7e2d086c77650b48f002cc51
tamaki <hbk2002320@gmail.com>
2021-02-16 23:57:09 +0900

    feat/edit-tasks

--- src/component/TaskItem.tsx
+++ src/component/TaskItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import firebase from 'firebase/app'
 import {ListItem, TextField, Grid} from '@material-ui/core'
 import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined'
@@ -10,12 +10,15 @@ interface PROPS {
 }
 
 const TaskItem: React.FC<PROPS> = (props) => {
+  const [title, setTitle] = useState(props.title)
+
   return (
-    <>
       <ListItem>
         <h2>{props.title}</h2>
+        <Grid container justify="flex-end">
+          <TextField InputLabelProps={{ shrink: true }} label="Edit task" value={title} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)} />
+        </Grid>
       </ListItem>
-    </>
   )
 }
 
