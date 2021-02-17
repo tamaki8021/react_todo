b87cc4bcb5c9381c47ed543b274cbfca11b6338e
tamaki <hbk2002320@gmail.com>
2021-02-17 16:43:26 +0900

    feat/edit-delete-button

--- src/component/TaskItem.tsx
+++ src/component/TaskItem.tsx
@@ -3,6 +3,7 @@ import firebase from 'firebase/app'
 import {ListItem, TextField, Grid} from '@material-ui/core'
 import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined'
 import EditOutlinedIcon from '@material-ui/icons/EditOutlined'
+import {db} from '../firebase'
 
 interface PROPS {
   id: string;
@@ -12,12 +13,26 @@ interface PROPS {
 const TaskItem: React.FC<PROPS> = (props) => {
   const [title, setTitle] = useState(props.title)
 
+  const editTask = () => {
+    db.collection("tasks").doc(props.id).set({title: title}, {merge: true})
+  }
+
+  const deleteTask = () => {
+    db.collection("tasks").doc(props.id).delete()
+  }
+
   return (
       <ListItem>
         <h2>{props.title}</h2>
         <Grid container justify="flex-end">
           <TextField InputLabelProps={{ shrink: true }} label="Edit task" value={title} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)} />
         </Grid>
+        <button onClick={editTask}>
+          <EditOutlinedIcon />
+        </button>
+        <button onClick={deleteTask}>
+          <DeleteOutlinedIcon />
+        </button>
       </ListItem>
   )
 }
